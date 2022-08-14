6ff5187768a054eb17c00aac766cb616914f8ee9
eldos <eldos.baktybekov@gmail.com>
2022-08-14 11:38:36 +0600

    :card_file_box: Get refresh token from database

--- src/repositories/access_tokens.py
+++ src/repositories/access_tokens.py
@@ -11,7 +11,7 @@ __all__ = (
 class TokensRepository(BaseRepository):
 
     async def get_by_account_name(self, account_name: str) -> models.Token:
-        tokens = await self._database.find_one({'account_name': account_name}, {'_id': 0, 'refresh_token': 0})
+        tokens = await self._database.find_one({'account_name': account_name}, {'_id': 0})
         if tokens is None:
             raise exceptions.NoTokenError
         return models.Token.parse_obj(tokens)
