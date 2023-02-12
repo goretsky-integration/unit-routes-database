cc0e98496d6de6b3bae5320af06bb19ddc1f1519
Eldos <eldos.baktybekov@gmail.com>
2023-02-12 21:38:44 +0600

    :construction: GI-10 Change base repository

--- src/repositories/__init__.py
+++
@@ -1,7 +0,0 @@
-from .access_tokens import *
-from .accounts import *
-from .cookies import *
-from .report_types import *
-from .reports import *
-from .statistics_report_types import *
-from .units import *
