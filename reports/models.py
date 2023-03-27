5afc0537b0502cf464e347b433231f86af2e70a8
Eldos <eldos.baktybekov@gmail.com>
2023-03-27 15:34:21 +0600

    :children_crossing: Change `ReportType` model repr (#GI-51)

--- reports/models.py
+++ reports/models.py
@@ -16,7 +16,7 @@ class ReportType(models.Model):
     is_active = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name
+        return self.verbose_name
 
 
 class ReportRoute(models.Model):
