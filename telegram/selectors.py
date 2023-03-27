f73632ee619a6e29d2ad7a622b4f258451c5288e
Eldos <eldos.baktybekov@gmail.com>
2023-03-27 18:05:37 +0600

    :construction: Add telegram chat selector (#GI-51)

--- telegram/selectors.py
+++ telegram/selectors.py
@@ -1,5 +1,6 @@
 from django.db.models import QuerySet
 
+from core.exceptions import NotFoundError
 from telegram.models import TelegramChat
 
 
@@ -9,3 +10,10 @@ def get_telegram_chats(*, limit: int, offset: int) -> QuerySet[TelegramChat]:
 
 def get_telegram_chats_by_chat_id(chat_id: int) -> QuerySet[TelegramChat]:
     return TelegramChat.objects.filter(chat_id=chat_id)
+
+
+def get_telegram_chat_by_chat_id(chat_id: int) -> TelegramChat:
+    try:
+        return TelegramChat.objects.get(chat_id=chat_id)
+    except TelegramChat.DoesNotExist:
+        raise NotFoundError('Chat by chat ID is not found')
