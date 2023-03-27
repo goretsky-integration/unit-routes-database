4f037fce16627f0e93371e5247e52b56972f1e17
Eldos <eldos.baktybekov@gmail.com>
2023-03-27 18:05:54 +0600

    :construction: Add report type selector (#GI-51)

--- reports/selectors.py
+++ reports/selectors.py
@@ -1,7 +1,15 @@
 from django.db.models import QuerySet
 
+from core.exceptions import NotFoundError
 from reports.models import ReportType
 
 
 def get_report_types() -> QuerySet[ReportType]:
     return ReportType.objects.all()
+
+
+def get_report_type_by_id(report_type_id: int) -> ReportType:
+    try:
+        return ReportType.objects.get(id=report_type_id)
+    except ReportType.DoesNotExist:
+        raise NotFoundError('Report type by ID is not found')
