82b65c5f8cd3cc314b04e23308c299a2afd6eb8e
Eldos <eldos.baktybekov@gmail.com>
2023-03-27 23:39:26 +0600

    :boom: Change units API endpoints (#GI-51)

--- units/views.py
+++ units/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from units.selectors import get_region_names, get_units, get_unit_by_name
+from units.selectors import get_regions, get_units, get_unit_by_name
 
 
 class UnitRetrieveByNameApi(APIView):
@@ -22,24 +22,43 @@ class UnitRetrieveByNameApi(APIView):
 class UnitsListApi(APIView):
 
     def get(self, request):
-        limit = request.query_params.get('limit', 100)
-        offset = request.query_params.get('skip', 0)
+        limit = int(request.query_params.get('limit', 100))
+        offset = int(request.query_params.get('skip', 0))
         units = get_units(limit=limit, offset=offset)
-        response_data = [
-            {
-                'id': unit.id,
-                'name': unit.name,
-                'uuid': unit.uuid,
-                'office_manager_account_name': unit.office_manager_account_name,
-                'dodo_is_api_account_name': unit.dodo_is_api_account_name,
-                'region': unit.region.name,
-            } for unit in units
-        ]
+        is_next_page_exists = get_units(limit=1, offset=limit + offset).exists()
+        response_data = {
+            'units': [
+                {
+                    'id': unit.id,
+                    'name': unit.name,
+                    'uuid': unit.uuid,
+                    'office_manager_account_name': unit.office_manager_account_name,
+                    'dodo_is_api_account_name': unit.dodo_is_api_account_name,
+                    'region': unit.region.name,
+                } for unit in units
+            ],
+            'is_end_of_list_reached': not is_next_page_exists,
+        }
         return Response(response_data)
 
 
 class UnitRegionsListApi(APIView):
 
     def get(self, request):
-        response_data = get_region_names()
+        limit = int(request.query_params.get('limit', 100))
+        offset = int(request.query_params.get('offset', 0))
+        regions = get_regions(limit=limit, offset=offset)
+        is_next_page_exists = get_regions(
+            limit=1,
+            offset=limit + offset,
+        ).exists()
+        response_data = {
+            'regions': [
+                {
+                    'id': region.id,
+                    'name': region.name,
+                } for region in regions
+            ],
+            'is_end_of_list_reached': not is_next_page_exists,
+        }
         return Response(response_data)
