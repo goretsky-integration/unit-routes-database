6d4f9dc8f338d7b0544adf67010bba082230a4ed
Eldos <eldos.baktybekov@gmail.com>
2023-03-27 18:05:05 +0600

    :goal_net: Add custom exception handler (#GI-51)

---
+++ core/views.py
@@ -0,0 +1,35 @@
+from rest_framework import exceptions as drf_api_exceptions
+from rest_framework.views import exception_handler as default_exception_handler
+
+from core.exceptions import ApplicationError, NotFoundError
+
+
+def exception_handler(exc, context):
+    """
+    HackSoft style guide https://github.com/HackSoftware/Django-Styleguide#errors--exception-handling.
+    {
+        "message": "Error message",
+        "extra": {}
+    }
+    """
+    match exc:
+        case NotFoundError():
+            exc = drf_api_exceptions.NotFound
+        case ApplicationError():
+            exc = drf_api_exceptions.APIException
+    response = default_exception_handler(exc, context)
+
+    if response is None:
+        return response
+
+    if isinstance(exc.detail, (list, dict)):
+        response.data = {'detail': response.data}
+    if isinstance(exc, drf_api_exceptions.ValidationError):
+        response.data['message'] = 'Validation error'
+        response.data["extra"] = {"fields": response.data["detail"]}
+    else:
+        response.data['message'] = response.data['detail']
+        response.data['extra'] = {}
+    del response.data['detail']
+
+    return response
