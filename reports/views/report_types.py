0706794e103bd958e0901767a3bf464b6124d156
Eldos <eldos.baktybekov@gmail.com>
2023-03-28 21:39:24 +0600

    :boom: Add pagination in report types endpoints (#GI-51)

--- reports/views/report_types.py
+++ reports/views/report_types.py
@@ -11,14 +11,35 @@ from reports.selectors import (
 class ReportTypesListApi(APIView):
 
     def get(self, request: Request):
-        report_types = get_active_report_types()
-        response_data = report_types.values('id', 'name', 'verbose_name')
+        limit = int(request.query_params.get('limit', 100))
+        offset = int(request.query_params.get('offset', 0))
+        report_types = get_active_report_types(limit=limit, offset=offset)
+        is_next_page_exists = get_active_report_types(
+            limit=1,
+            offset=limit + offset,
+        ).exists()
+        response_data = {
+            'report_types': report_types.values('id', 'name', 'verbose_name'),
+            'is_end_of_list_reached': not is_next_page_exists,
+        }
         return Response(response_data)
 
 
 class StatisticsReportTypesListApi(APIView):
 
     def get(self, request: Request):
-        report_types = get_active_statistics_report_types()
-        response_data = report_types.values('id', 'name', 'verbose_name')
+        limit = int(request.query_params.get('limit', 100))
+        offset = int(request.query_params.get('offset', 0))
+        report_types = get_active_statistics_report_types(
+            limit=limit,
+            offset=offset,
+        )
+        is_next_page_exists = get_active_statistics_report_types(
+            limit=1,
+            offset=limit + offset,
+        ).exists()
+        response_data = {
+            'report_types': report_types.values('id', 'name', 'verbose_name'),
+            'is_end_of_list_reached': not is_next_page_exists,
+        }
         return Response(response_data)
