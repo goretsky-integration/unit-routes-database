0b4f83ba54bb3c8cb09c6ecdf98a4d647bfa51dc
Eldos <eldos.baktybekov@gmail.com>
2023-03-28 21:19:50 +0600

    :sparkles: Add import_export plugin to units admin (#GI-51)

--- units/admin.py
+++ units/admin.py
@@ -1,8 +1,18 @@
+from django.conf import settings
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from core.mixins import OnlyDebugAddChangeDeleteMixin
 from units.forms import UnitWithIdForm
 from units.models import Region, Unit
+
+ModelAdmin = ImportExportModelAdmin if settings.DEBUG else admin.ModelAdmin
+
+
+class UnitResource(resources.ModelResource):
+    class Meta:
+        model = Unit
 
 
 class UnitInline(admin.TabularInline):
@@ -15,6 +25,9 @@ class RegionAdmin(OnlyDebugAddChangeDeleteMixin, admin.ModelAdmin):
 
 
 @admin.register(Unit)
-class UnitAdmin(OnlyDebugAddChangeDeleteMixin, admin.ModelAdmin):
+class UnitAdmin(OnlyDebugAddChangeDeleteMixin, ModelAdmin):
     form = UnitWithIdForm
     list_filter = ('region',)
+
+    if settings.DEBUG:
+        resource_class = UnitResource
