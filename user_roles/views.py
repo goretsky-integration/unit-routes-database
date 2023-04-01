86f05c3259fa34ebb353c7043467f1eb0bb92591
Eldos <eldos.baktybekov@gmail.com>
2023-04-01 17:15:32 +0600

    :sparkles: Add `region_id` in units list api (#GI-51)

--- user_roles/views.py
+++ user_roles/views.py
@@ -46,7 +46,7 @@ class UserRoleUnitsListApi(APIView):
             units = units.filter(region_id=region_id)
         units = (
             units.order_by('id')[offset:limit + 1 + offset]
-            .values('id', 'name', 'uuid')
+            .values('id', 'name', 'uuid', 'region_id')
         )
         is_next_page_exists = len(units) == limit + 1
         if is_next_page_exists:
