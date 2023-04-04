f9516ea2bfe22684143cdd02977cc64c7a37caa0
Eldos <eldos.baktybekov@gmail.com>
2023-04-04 09:16:08 +0600

    :bug: Add support for negative numbers in url path (#GI-51)

--- user_roles/urls.py
+++ user_roles/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, register_converter
 
 from user_roles.views import (
     UserRoleUnitsListApi,
@@ -6,15 +6,17 @@ from user_roles.views import (
     UserRoleReportTypesListApi,
     SetUserRoleApi,
 )
+from core.converters import AnyIntConverter
+
+register_converter(AnyIntConverter, 'any_int')
 
 user_roles_urlpatterns = [
     path('regions/', UserRoleRegionsListApi.as_view()),
     path('report-types/', UserRoleReportTypesListApi.as_view()),
     path('units/', UserRoleUnitsListApi.as_view()),
     path('', SetUserRoleApi.as_view()),
-
 ]
 
 urlpatterns = [
-    path('users/<int:chat_id>/', include(user_roles_urlpatterns)),
+    path('users/<any_int:chat_id>/', include(user_roles_urlpatterns)),
 ]
