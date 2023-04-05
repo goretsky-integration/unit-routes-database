cf785f5309571ae2e53484c8160d48cb0ef0a10c
Eldos <eldos.baktybekov@gmail.com>
2023-04-05 19:15:09 +0600

    :children_crossing: Add telegram chat admin features (#GI-53)

--- telegram/admin.py
+++ telegram/admin.py
@@ -5,4 +5,7 @@ from telegram.models import TelegramChat
 
 @admin.register(TelegramChat)
 class TelegramChatAdmin(admin.ModelAdmin):
-    list_filter = ('type',)
+    list_filter = ('type', 'role')
+    list_display = ('title', 'username', 'type', 'role', 'created_at')
+    list_select_related = ('role',)
+    readonly_fields = ('created_at', 'chat_id', 'type')
