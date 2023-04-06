b07cad4d7785dea6be1926fd89137b2593726186
Eldos <eldos.baktybekov@gmail.com>
2023-04-06 22:59:07 +0600

    :children_crossing: TelegramChat admin improvements (#GI-53)
    * Handle telegram chat role modification.
    * Remove report routes which are not allowed to the role at telegram chat modification.
    * Remove permission to add telegram chats.

--- telegram/admin.py
+++ telegram/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
 from django.utils.text import capfirst
+from django.utils.translation import gettext_lazy as _
 
 from telegram.models import TelegramChat
+from user_roles.services import update_user_role
 
 
 @admin.register(TelegramChat)
@@ -15,3 +16,17 @@ class TelegramChatAdmin(admin.ModelAdmin):
     search_help_text = capfirst(
         _('telegram|admin|telegram_chat|search_help_text')
     )
+
+    def has_add_permission(self, request):
+        return False
+
+    def save_model(self, request, obj: TelegramChat, form, change):
+        if obj.id is not None:
+            telegram_chat = (
+                TelegramChat.objects
+                .select_related('role')
+                .get(id=obj.id)
+            )
+            if telegram_chat.role != obj.role:
+                update_user_role(user=telegram_chat, role=obj.role)
+        super().save_model(request, obj, form, change)
