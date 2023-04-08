cf6b31b5e908462c5ace9f32541f389bc558e671
Eldos <eldos.baktybekov@gmail.com>
2023-04-08 13:25:48 +0600

    :card_file_box: Add `Department` model (#GI-54)

--- units/models.py
+++ units/models.py
@@ -3,6 +3,29 @@ from django.utils.text import capfirst
 from django.utils.translation import gettext_lazy as _
 
 
+class Department(models.Model):
+    id = models.PositiveSmallIntegerField(
+        primary_key=True,
+        verbose_name=_('units|model|department|id'),
+    )
+    name = models.CharField(
+        max_length=64,
+        unique=True,
+        verbose_name=_('units|model|department|name'),
+    )
+    uuid = models.UUIDField(
+        unique=True,
+        verbose_name=_('units|model|department|uuid'),
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('units|model|department')
+        verbose_name_plural = _('units|model|departments')
+
+
 class Region(models.Model):
     name = models.CharField(
         max_length=64,
@@ -44,6 +67,11 @@ class Unit(models.Model):
         to=Region,
         on_delete=models.CASCADE,
         verbose_name=capfirst(_('units|model|unit|region')),
+    )
+    department = models.ForeignKey(
+        to=Department,
+        on_delete=models.CASCADE,
+        verbose_name=_('units|model|unit|department'),
     )
 
     def __str__(self):
