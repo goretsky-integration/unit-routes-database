3543c6bf377f3721e237dee656ac8f782eb75d24
Eldos <eldos.baktybekov@gmail.com>
2023-04-22 01:45:50 +0600

    :sparkles: Add service to send requests to dodo is auth system (#GI-61)

---
+++ dodo_is_auth/auth.py
@@ -0,0 +1,53 @@
+from dodo_is_auth.models import (
+    HTTPClient,
+    ConnectAuthorizeFormData,
+    HTML,
+    FilledAccountLoginFormData,
+)
+
+
+class DodoISAuthService:
+
+    def __init__(self, http_client: HTTPClient):
+        self._http_client = http_client
+
+    def send_connect_authorize_form_data(
+            self,
+            connect_authorize_form_data: ConnectAuthorizeFormData,
+    ) -> HTML:
+        request_data = {
+            'client_id': connect_authorize_form_data.client_id,
+            'redirect_uri': connect_authorize_form_data.redirect_uri,
+            'response_type': connect_authorize_form_data.response_type,
+            'scope': connect_authorize_form_data.scope,
+            'code_challenge': connect_authorize_form_data.code_challenge,
+            'code_challenge_method': connect_authorize_form_data.code_challenge_method,
+            'response_mode': connect_authorize_form_data.response_mode,
+            'nonce': connect_authorize_form_data.nonce,
+            'state': connect_authorize_form_data.state,
+        }
+        url = '/connect/authorize'
+        response = self._http_client.post(url, data=request_data)
+        return HTML(response.text)
+
+    def send_account_login_form_data(
+            self,
+            *,
+            account_login_form_data: FilledAccountLoginFormData,
+    ) -> HTML:
+        request_data = {
+            'ReturnUrl': account_login_form_data.return_url,
+            'Username': account_login_form_data.username,
+            'Password': account_login_form_data.password,
+            'TenantName': account_login_form_data.tenant_name,
+            'CountryCode': account_login_form_data.country_code,
+            'authMethod': account_login_form_data.auth_method,
+            '__RequestVerificationToken': account_login_form_data.request_verification_token,
+            'RememberLogin': account_login_form_data.remember_login,
+        }
+        url = '/account/login'
+        response = self._http_client.post(
+            url=url,
+            data=request_data,
+        )
+        return HTML(response.text)
