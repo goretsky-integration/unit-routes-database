48b3ee983da7eeed03246041b7b1be07291cfb73
Eldos <eldos.baktybekov@gmail.com>
2025-06-03 22:31:00 +0600

    :wrench: Enhance notification command to include chat IDs for write-offs

--- write_offs/management/commands/send_notifications.py
+++ write_offs/management/commands/send_notifications.py
@@ -1,5 +1,8 @@
 from django.core.management import BaseCommand
+from django.db.models import QuerySet
 
+from reports.models.report_routes import ReportRoute
+from write_offs.models import IngredientWriteOff
 from write_offs.services import (
     get_expired_repeating_write_offs,
     get_upcoming_write_offs,
@@ -9,5 +12,13 @@ from write_offs.services import (
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        write_offs = get_upcoming_write_offs() + get_expired_repeating_write_offs()
+        write_offs: QuerySet[IngredientWriteOff] = (
+                get_upcoming_write_offs()
+                + get_expired_repeating_write_offs()
+        )
+
+        for write_off in write_offs:
+            chat_ids = ReportRoute.objects.filter(unit_id=write_off.unit_id).values_list('telegram_chat__chat_id', flat=True)
+
+            for chat_id in chat_ids:
 
