0a167d433115e1a7c120b5541f53a0257d2bf088
Eldos <eldos.baktybekov@gmail.com>
2025-10-29 14:48:07 +0600

    :wrench: Add filtering functions for inventory stock items and update dependencies

--- reports/services.py
+++ reports/services.py
@@ -192,3 +192,50 @@ def get_dodo_is_api_gateway(
         timeout=timeout,
     ) as http_client:
         yield DodoIsApiGateway(http_client=http_client)
+
+
+def is_all_zero(*numbers: int | float) -> bool:
+    return all(number == 0 for number in numbers)
+
+
+def filter_relevant_items(
+        items: Iterable[InventoryStockItem],
+) -> list[InventoryStockItem]:
+    relevant_items: list[InventoryStockItem] = []
+
+    for item in items:
+        if '(не использовать)' in item.name.lower():
+            continue
+        if is_all_zero(
+            item.average_weekend_expense,
+            item.average_weekday_expense,
+            item.balance_in_money,
+            item.quantity,
+            item.days_until_balance_runs_out,
+        ):
+            continue
+
+        relevant_items.append(item)
+
+    return relevant_items
+
+
+def filter_running_out_stock_items(
+        items: Iterable[InventoryStockItem],
+        threshold: int,
+) -> list[InventoryStockItem]:
+    return [
+        item for item in items
+        if item.days_until_balance_runs_out <= threshold
+    ]
+
+
+def filter_by_category_names(
+        items: Iterable[InventoryStockItem],
+        category_names: Iterable[InventoryStockCategoryName],
+) -> list[InventoryStockItem]:
+    category_names = set(category_names)
+    return [
+        item for item in items
+        if item.category_name in category_names
+    ]
