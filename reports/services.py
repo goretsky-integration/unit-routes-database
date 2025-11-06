cd6c2cc24990b7663f70deef2e8562b04b391ca3
Eldos <eldos.baktybekov@gmail.com>
2025-11-06 20:26:35 +0600

    :wrench: Implement report generation for stop sales by sales channels

--- reports/services.py
+++ reports/services.py
@@ -8,14 +8,16 @@ from collections.abc import Iterable, Generator, Mapping
 from dataclasses import dataclass
 from enum import StrEnum, auto
 from itertools import batched
-from typing import Annotated, Any
+from typing import Annotated, Any, Self, TypeAlias
 from typing import NewType, ClassVar, Final
 from uuid import UUID
 
 import gspread
 import httpx
+import humanize
 from django.conf import settings
 from django.db.utils import IntegrityError
+from django.utils import timezone
 from gspread.utils import ValueInputOption
 from pydantic import BaseModel, Field, ValidationError
 from pydantic import computed_field, field_validator
@@ -659,6 +661,7 @@ class CountryCode(StrEnum):
 class SalesChannel(StrEnum):
     DELIVERY = auto()
     DINE_IN = auto()
+    TAKEAWAY = auto()
 
 
 class DetailedOrderDelivery(BaseModel):
@@ -842,3 +845,211 @@ class CanceledOrdersGoogleSheetsGateway:
             value_input_option=ValueInputOption.user_entered,
         )
         time.sleep(2.5)
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class Period:
+    start: datetime.datetime
+    end: datetime.datetime
+
+    @classmethod
+    def today_to_this_time(cls) -> Self:
+        now = timezone.localtime()
+        return cls(
+            start=now.replace(
+                hour=0,
+                minute=0,
+                second=0,
+                microsecond=0,
+            ),
+            end=now,
+        )
+
+    @classmethod
+    def today_full_day(cls) -> Self:
+        now = timezone.localtime()
+        return cls(
+            start=now.replace(
+                hour=0,
+                minute=0,
+                second=0,
+                microsecond=0,
+            ),
+            end=now.replace(
+                hour=23,
+                minute=59,
+                second=59,
+                microsecond=999999,
+            ),
+        )
+
+    @classmethod
+    def yesterday_to_this_time(cls) -> Self:
+        today = cls.today_to_this_time()
+        return cls(
+            start=today.start - datetime.timedelta(days=1),
+            end=today.end - datetime.timedelta(days=1),
+        )
+
+
+def filter_not_ended_stop_sales(
+    stop_sales: Iterable[StopSaleBySalesChannel],
+) -> list[StopSaleBySalesChannel]:
+    return [
+        stop_sale for stop_sale in stop_sales
+        if stop_sale.ended_at_local is None
+    ]
+
+
+def filter_by_sales_channels(
+    stop_sales: Iterable[StopSaleBySalesChannel],
+) -> list[StopSaleBySalesChannel]:
+    sales_channels = {
+        SalesChannel.DINE_IN,
+        SalesChannel.DELIVERY,
+    }
+    return [
+        stop_sale for stop_sale in stop_sales
+        if stop_sale.sales_channel in sales_channels
+    ]
+
+
+def filter_complete_stop_sales(
+    stop_sales: Iterable[StopSaleBySalesChannel],
+) -> list[StopSaleBySalesChannel]:
+    return [
+        stop_sale for stop_sale in stop_sales
+        if stop_sale.channel_stop_type == ChannelStopType.COMPLETE
+    ]
+
+
+def filter_stop_sales_by_sales_channels(
+    stop_sales: Iterable[StopSaleBySalesChannel],
+) -> list[StopSaleBySalesChannel]:
+    stop_sales = filter_not_ended_stop_sales(stop_sales)
+    stop_sales = filter_by_sales_channels(stop_sales)
+    stop_sales = filter_complete_stop_sales(stop_sales)
+    return stop_sales
+
+
+SALES_CHANNEL_TO_NAME = {
+    SalesChannel.DINE_IN: 'Ресторан',
+    SalesChannel.TAKEAWAY: 'Самовывоз',
+    SalesChannel.DELIVERY: 'Доставка',
+}
+
+MINUTE_IN_SECONDS: Final[int] = 60
+HOUR_IN_SECONDS: Final[int] = MINUTE_IN_SECONDS * 60
+DAY_IN_SECONDS: Final[int] = HOUR_IN_SECONDS * 24
+
+TimeUnitsAndAbbreviation: TypeAlias = tuple[tuple[str, ...], str]
+
+
+def abbreviate_time_units(text: str) -> str:
+    time_units_and_abbreviations: tuple[TimeUnitsAndAbbreviation, ...] = (
+        (('дней', 'день', 'дня'), 'дн'),
+        (('часов', 'часа', 'час',), 'ч'),
+        (('минута', 'минуты', 'минут'), 'мин'),
+    )
+    words = set(text.split())
+    for time_units, abbreviation in time_units_and_abbreviations:
+        for time_unit in time_units:
+            if time_unit not in words:
+                continue
+            text = text.replace(time_unit, abbreviation)
+    return text
+
+
+def compute_duration(started_at: datetime.datetime) -> datetime.timedelta:
+    return timezone.localtime() + datetime.timedelta(hours=3) - started_at
+
+
+def humanize_stop_sale_duration(duration: datetime.timedelta) -> str:
+    stop_duration_in_seconds = duration.total_seconds()
+
+    if stop_duration_in_seconds >= DAY_IN_SECONDS:
+        kwargs = {
+            'format': '%0.0f',
+            'minimum_unit': 'days',
+            'suppress': ['months'],
+        }
+    elif stop_duration_in_seconds >= HOUR_IN_SECONDS:
+        kwargs = {'format': '%0.0f', 'minimum_unit': 'hours'}
+    elif stop_duration_in_seconds >= MINUTE_IN_SECONDS:
+        kwargs = {'format': '%0.0f', 'minimum_unit': 'minutes'}
+    else:
+        kwargs = {'format': '%0.0f', 'minimum_unit': 'seconds'}
+
+    return abbreviate_time_units(humanize.precisedelta(duration, **kwargs))
+
+
+def is_urgent(duration: datetime.timedelta) -> bool:
+    return duration.total_seconds() >= MINUTE_IN_SECONDS * 30
+
+
+def humanize_seconds(seconds: int) -> str:
+    """Humanize time in seconds.
+
+    Examples:
+        >>> humanize_seconds(60)
+        '01:00'
+        >>> humanize_seconds(0)
+        '00:00'
+        >>> humanize_seconds(3600)
+        '01:00:00'
+        >>> humanize_seconds(9000000)
+        '+99:59:59'
+
+    Args:
+        seconds: Time in seconds.
+
+    Returns:
+        Humanized time in HH:MM:SS or MM:SS format.
+        If there are over 100 hours (359999 seconds), returns "+99:59:59".
+    """
+    if seconds > 359999:
+        return '+99:59:59'
+    minutes = seconds // 60
+    seconds %= 60
+    hours = minutes // 60
+    minutes %= 60
+    if not hours:
+        return f'{minutes:02}:{seconds:02}'
+    return f'{hours:02}:{minutes:02}:{seconds:02}'
+
+
+def render_stop_sale_header(
+    *,
+    unit_name: str,
+    started_at: datetime.datetime,
+):
+    stop_sale_duration = compute_duration(started_at)
+    humanized_stop_sale_duration = humanize_stop_sale_duration(
+        duration=stop_sale_duration,
+    )
+    humanized_stop_sale_started_at = f'{started_at:%H:%M}'
+
+    header = (
+        f'{unit_name}'
+        f' в стопе {humanized_stop_sale_duration}'
+        f' (с {humanized_stop_sale_started_at})')
+
+    if is_urgent(stop_sale_duration):
+        header = '❗️ ' + header + ' ❗️'
+
+    return header
+
+
+def format_stop_sale_by_sales_channel(
+    stop_sale: StopSaleBySalesChannel,
+) -> str:
+    header = render_stop_sale_header(
+        unit_name=stop_sale.unit_name,
+        started_at=stop_sale.started_at,
+    )
+    channel_name = SALES_CHANNEL_TO_NAME[stop_sale.sales_channel]
+    return (
+        f'{header}\n'
+        f'Тип продажи: {channel_name}\n'
+        f'Причина: {stop_sale.reason}'
+    )
---
+++ reports/use_cases/create_stop_sales_by_sales_channels_report.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+
+from accounts.models import AccountTokens
+from accounts.services.crypt import decrypt_string
+from reports.models.report_routes import ReportRoute
+from reports.services import (
+    get_dodo_is_api_gateway, Period,
+    filter_stop_sales_by_sales_channels, format_stop_sale_by_sales_channel,
+)
+from telegram.services import batch_create_telegram_messages
+from units.models import Unit
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class CreateStopSalesBySalesChannelsReport:
+
+    def execute(self) -> None:
+        accounts_tokens = AccountTokens.objects.select_related('account').all()
+        today = Period.today_to_this_time()
+
+        for account_token in accounts_tokens:
+            units = Unit.objects.filter(
+                dodo_is_api_account_name=account_token.account.name,
+            )
+            unit_ids = {unit.uuid for unit in units}
+
+            access_token = decrypt_string(account_token.encrypted_access_token)
+
+            with get_dodo_is_api_gateway(
+                access_token=access_token,
+            ) as dodo_is_api_gateway:
+                stop_sales = dodo_is_api_gateway.get_stop_sales_by_sales_channels(
+                    unit_ids=unit_ids,
+                    date_from=today.start,
+                    date_to=today.end,
+                )
+
+            stop_sales = filter_stop_sales_by_sales_channels(stop_sales)
+
+            for stop_sale in stop_sales:
+                text = format_stop_sale_by_sales_channel(stop_sale)
+
+                chat_ids = (
+                    ReportRoute.objects
+                    .filter(
+                        report_type__name='PIZZERIA_STOP_SALES',
+                        unit__uuid=stop_sale.unit_id,
+                    )
+                    .values_list('telegram_chat__chat_id', flat=True)
+                )
+                batch_create_telegram_messages(
+                    chat_ids=chat_ids,
+                    text=text,
+                )
