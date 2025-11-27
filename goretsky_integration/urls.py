99ff36f71eb9da5679c61731fe9a08b0ba5d7ff3
Eldos <eldos.baktybekov@gmail.com>
2025-11-27 16:43:20 +0600

    Update URL patterns to include 'api/' prefix for improved API structure

--- goretsky_integration/urls.py
+++ goretsky_integration/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = i18n_patterns(
-    path('admin/', admin.site.urls),
-    path('units/', include('units.urls')),
-    path('telegram-chats/', include('telegram.urls')),
-    path('roles/', include('user_roles.urls')),
-    path('', include('accounts.urls')),
-    path('', include('reports.urls')),
-    path('write-offs/', include('write_offs.urls')),
-    prefix_default_language=False,
-)
+
+urlpatterns = [
+    path('api/admin/', admin.site.urls),
+    path('api/units/', include('units.urls')),
+    path('api/telegram-chats/', include('telegram.urls')),
+    path('api/roles/', include('user_roles.urls')),
+    path('api/', include('accounts.urls')),
+    path('api/', include('reports.urls')),
+    path('api/write-offs/', include('write_offs.urls')),
+]
